65e5c665859c51308ba22df2418a5b7a03a94df2
LP <lp.passarelli.wilson@gmail.com>
2022-12-01 11:34:42 -0800

    fixed the test, implemented fizzbuzz

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,8 +1,11 @@
 export const fizzBuzz = (num) => {
+  console.log(`I got to this point and num is ${num}`);
   if (num % 15 === 0) {
     return 'FizzBuzz';
   } else if (num % 5 === 0) {
     return 'Buzz';
+  } else if (num % 3 === 0) {
+    return 'Fizz';
   } else {
     return num;
   }
